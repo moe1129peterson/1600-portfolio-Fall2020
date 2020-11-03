b1c9ff07a2bdac751e90c9ed07a2b4ab913c090f
moe1129peterson <moe1129tsuzuki@gmail.com>
2020-11-03 10:31:28 -0700

    11_03 DGM1660

---
+++ star-wars-character/main.js
@@ -0,0 +1,75 @@
+import { people } from '../data/people.js'
+
+const mainContent = document.querySelector('#main')
+
+const mainHeader = document.createElement('header')
+mainHeader.className = 'mainHeader'
+document.body.insertBefore(mainHeader, mainContent)
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+mainHeader.appendChild(maleButton)
+
+const femaleButton = document.createElement('button')
+femaleButton.textContent = 'Female Characters'
+mainHeader.appendChild(femaleButton)
+
+const otherButton = document.createElement('button')
+otherButton.textContent = 'Other Characters'
+mainHeader.appendChild(otherButton)
+
+const maleCharacters = people.filter((person) => person.gender === 'male')
+
+const femaleCharacters = people.filter((person) => person.gender === 'female')
+
+const otherCharacters = people.filter((thing) => {
+    if (
+        thing.gender === 'n/a' || 
+        thing.gender === 'none' ||
+        thing.gender === 'hermaphrodite'
+        ) {
+        return thing
+    }
+})
+
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+function populateDOM(characters) {
+    removeChildren(mainContent)
+    characters.forEach((element) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error', () => charImg.hidden = true) // genius level
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+    
+        mainContent.appendChild(charFigure)
+    })
+}
+
+// let theUrl = "https://swapi.co/api/people/2/"
+// let theUrl2 = "https://swapi.co/api/people/12/"
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+function removeChildren(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+}
